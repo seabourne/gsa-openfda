e4796213299636491dc6b72facbf75eb64ba6023
Mike Reich <mjreich@gmail.com>
2015-07-01 14:51:33 -0500

    Updated testing script

--- test.sh
+++ test.sh
@@ -2,8 +2,40 @@
 # @Author: Mike Reich
 # @Date:   2015-07-01 02:56:07
 # @Last Modified by:   Mike Reich
-# @Last Modified time: 2015-07-01 02:56:25
+# @Last Modified time: 2015-07-01 14:49:28
 
 set -e
 
 echo "Running tests"
+
+echo "Testing Core"
+cd ./plugins/core
+npm test
+
+echo "Testing Client"
+cd ../client
+npm test
+
+echo "Testing action manager"
+cd ../action-manager
+npm test
+
+echo "Testing entity extraction"
+cd ../entity-extraction
+npm test
+
+echo "Testing logging"
+cd ../logging
+npm test
+
+echo "Testing openfda-source"
+cd ../openfda-source
+npm test
+
+echo "Testing router"
+cd ../router
+npm test
+
+echo "Testing storage-mongoose"
+cd ../storage-mongoose
+npm test
