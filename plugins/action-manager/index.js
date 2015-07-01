984529c8a8a0af44052cae7c9fc5d78720089ed1
Mike Reich <mjreich@gmail.com>
2015-07-01 02:58:41 -0500

    Removed unused startup script

--- plugins/action-manager/index.js
+++ plugins/action-manager/index.js
@@ -1,27 +1,14 @@
 /* 
 * @Author: Mike Reich
 * @Date:   2015-06-30 07:28:22
-* @Last Modified 2015-06-30
+* @Last Modified 2015-07-01
 */
 
 'use strict';
 
 function ActionManager(app, loaded) {
-  app._bootAwait['app.startup'].push('actions.ready')
   
   require("./lib/Action")(this, app)
-
-  app.on('app.startup', () => {
-    if(app.config.RESET_CONTENT) {
-      app.emit('storage.getModel', 'Action', (err, A) => {
-        A.remove({}, (err, n) => {
-          app.log.debug(n+" actions removed")
-          app.emit('actions.ready')
-        })
-      })
-    } //else
-      //app.emit('actions.ready')
-  })
 
   app.on('entity.record', (entity) => {
     app.emit('storage.getModel', 'Action', (err, Action) => {
